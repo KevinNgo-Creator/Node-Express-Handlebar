7b875ea053be01bc13b0ea097b919440d5f68c2b
Kevin Ngo <ltn8rd@mail.umkc.edu>
2020-03-12 10:37:14 -0500

    touch up

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "",
+        password: "Bear!8553917",
         database: "burgers_db"
     });
 };
